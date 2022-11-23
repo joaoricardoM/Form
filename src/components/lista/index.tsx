08899dcbec5c510035d5ea836a6958f8423476aa
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-11-23 16:23:17 -0300

    format documents

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -1,32 +1,43 @@
-import React, { useState } from 'react';
-import style from './lista.module.scss'
-import Item from './item'
+import React, { useState } from "react";
+import style from "./lista.module.scss";
+import Item from "./item";
 
 function Lista() {
-    const [tarefas, setTarefas] = useState([{
-        tarefa: 'Spring Boot',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'Scripts',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typecript',
-        tempo: '01:00:00'
-    }]);
+  const [tarefas, setTarefas] = useState([
+    {
+      tarefa: "Spring Boot",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Scripts",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "Typecript",
+      tempo: "01:00:00",
+    },
+  ]);
 
-    return (
-        <aside className={style.listaTarefas}>
-            <h2 onClick={() =>  setTarefas([...tarefas, { tarefa: "Estudar Estudo", tempo: "05:00:00" }])}> Study of the day </h2>
-            <ul>
-                {tarefas.map((item, index) => (
-                    <Item
-                        key={index}
-                        {...item}
-                    />
-                ))}
-            </ul>
-        </aside>
-    )
+  return (
+    <aside className={style.listaTarefas}>
+      <h2
+        onClick={() =>
+          setTarefas([
+            ...tarefas,
+            { tarefa: "Estudar Estudo", tempo: "05:00:00" },
+          ])
+        }
+      >
+        {" "}
+        Study of the day{" "}
+      </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default Lista;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Cronometro } from '../components/cronometro';
-import Formulario from '../components/formulario';
-import Lista from '../components/lista'
-import style from './App.module.scss'
+import React from "react";
+import { Cronometro } from "../components/cronometro";
+import Formulario from "../components/formulario";
+import Lista from "../components/lista";
+import style from "./App.module.scss";
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
       <Lista />
-      <Cronometro/>
+      <Cronometro />
     </div>
   );
 }
